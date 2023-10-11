062e01f1d684f2dff2f94767cb6926017c7c2d73
spikeyarmaku <spikeyarmaku@gmail.com>
2023-10-11 11:56:19 +0200

    Add drag and zoom to tree view

--- scripts/tree_container.gd
+++ scripts/tree_container.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var _dragging : bool = false
+
 const Closure = preload("res://vm/templates/closure.tscn")
 
 func deserialize(serializer : Serializer):
@@ -7,3 +9,17 @@ func deserialize(serializer : Serializer):
 
 func reset():
     %Tree.reset()
+
+func _on_sub_viewport_container_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        match event.button_index:
+            MOUSE_BUTTON_LEFT:
+                _dragging = event.pressed
+            MOUSE_BUTTON_WHEEL_UP:
+                %Camera2D.zoom *= 1.2
+            MOUSE_BUTTON_WHEEL_DOWN:
+                %Camera2D.zoom /= 1.2
+        print(event)
+    elif event is InputEventMouseMotion:
+        if _dragging:
+            %Camera2D.position -= event.relative / %Camera2D.zoom
